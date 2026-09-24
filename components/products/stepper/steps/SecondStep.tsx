d9f6075464ce1c9ae06f2b0625f80b1b0977037c
zyzik1248 <zuzannadudzinska96@gmail.com>
2026-09-24 11:52:01 +0200

    fix step2

--- components/products/stepper/steps/SecondStep.tsx
+++ components/products/stepper/steps/SecondStep.tsx
@@ -32,83 +32,79 @@ export default function SecondStep({
 
                 return (
                     <form >
-                        <form.Subscribe
-                            selector={(state: any) => state.values.step2}
-                        >
-                            <div className="flex flex-col gap-4">
-                                <div className="flex flex-col gap-4 md:flex-row">
-                                    <ProductInput
-                                        form={form}
-                                        label="Cena netto"
-                                        name="priceNet"
-                                        placeholder="0.0"
-                                        groupName="step2"
-                                        schema={step2Schema}
-                                        type="number"
-                                        onChange={(e) => {
-                                            const net = Number(e)
-                                            const vat = Number(form.getFieldValue("step2.vat"))
+                        <div className="flex flex-col gap-4">
+                            <div className="flex flex-col gap-4 md:flex-row">
+                                <ProductInput
+                                    form={form}
+                                    label="Cena netto"
+                                    name="priceNet"
+                                    placeholder="0.0"
+                                    groupName="step2"
+                                    schema={step2Schema}
+                                    type="number"
+                                    onChange={(e) => {
+                                        const net = Number(e)
+                                        const vat = Number(form.getFieldValue("step2.vat"))
 
-                                            form.setFieldValue(
-                                                "step2.priceGross",
-                                                String(
-                                                    Math.round((net * (1 + vat / 100)) * 100) / 100
-                                                )
+                                        form.setFieldValue(
+                                            "step2.priceGross",
+                                            String(
+                                                Math.round((net * (1 + vat / 100)) * 100) / 100
                                             )
-                                        }}
-                                    />
-                                    <ProductInput
-                                        form={form}
-                                        label="Cena brutto"
-                                        name="priceGross"
-                                        placeholder="0.0"
-                                        groupName="step2"
-                                        schema={step2Schema}
-                                        type="number"
-                                        onChange={(e) => {
-                                            const gross = Number(e)
-                                            const vat = Number(form.getFieldValue("step2.vat"))
+                                        )
+                                    }}
+                                />
+                                <ProductInput
+                                    form={form}
+                                    label="Cena brutto"
+                                    name="priceGross"
+                                    placeholder="0.0"
+                                    groupName="step2"
+                                    schema={step2Schema}
+                                    type="number"
+                                    onChange={(e) => {
+                                        const gross = Number(e)
+                                        const vat = Number(form.getFieldValue("step2.vat"))
 
-                                            form.setFieldValue(
-                                                "step2.priceNet",
-                                                String(
-                                                    Math.round((gross / (1 + vat / 100)) * 100) / 100
-                                                )
+                                        form.setFieldValue(
+                                            "step2.priceNet",
+                                            String(
+                                                Math.round((gross / (1 + vat / 100)) * 100) / 100
                                             )
-                                        }}
-                                    />
-                                </div>
-                                <div className="flex flex-col gap-4 md:flex-row">
-                                    <ProductInput
-                                        form={form}
-                                        label="Stawka VAT"
-                                        name="vat"
-                                        placeholder="23%"
-                                        groupName="step2"
-                                        schema={step2Schema}
-                                        suffix="%"
-                                        onChange={(e) => {
-                                            const vat = Number(e)
-                                            const net = Number(form.getFieldValue("step2.priceNet"))
-
-                                            form.setFieldValue(
-                                                "step2.priceGross",
-                                                String(Math.round(net * (1 + vat / 100)))
-                                            )
-                                        }}
-                                    />
-                                    <ProductSelect
-                                        form={form}
-                                        label="Waluta"
-                                        name="currency"
-                                        placeholder="PLN"
-                                        groupName="step2"
-                                        schema={step2Schema}
-                                        items={currencies.map(({ id, name }) => ({ label: name, value: id }))}
-                                    />
-                                </div>
+                                        )
+                                    }}
+                                />
                             </div>
-                        </form.Subscribe>
+                            <div className="flex flex-col gap-4 md:flex-row">
+                                <ProductInput
+                                    form={form}
+                                    label="Stawka VAT"
+                                    name="vat"
+                                    placeholder="23%"
+                                    groupName="step2"
+                                    schema={step2Schema}
+                                    suffix="%"
+                                    onChange={(e) => {
+                                        const vat = Number(e)
+                                        const net = Number(form.getFieldValue("step2.priceNet"))
+
+                                        form.setFieldValue(
+                                            "step2.priceGross",
+                                            String(Math.round(net * (1 + vat / 100)))
+                                        )
+                                    }}
+                                />
+                                <ProductSelect
+                                    form={form}
+                                    label="Waluta"
+                                    name="currency"
+                                    placeholder="PLN"
+                                    groupName="step2"
+                                    schema={step2Schema}
+                                    items={currencies.map(({ id, name }) => ({ label: name, value: id }))}
+                                />
+                            </div>
+                        </div>
                     </form >
                 )
             }}
